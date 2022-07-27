1a84ac9ae2549eaa933f79fbee2bc94049319457
Miguel <miguelzimmerman.leugim@gmail.com>
2022-07-27 20:53:36 -0300

    aula de fundamentos de NextJS

---
+++ src/pages/api/jsx.jsx
@@ -0,0 +1,7 @@
+export default function Jsx() {
+  return (
+    <div>
+      <h1>JSX é um conceito central</h1>
+    </div>
+  )
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Fundamentos de NextJs</h1>
+    </div>
+  
+    )
+}
